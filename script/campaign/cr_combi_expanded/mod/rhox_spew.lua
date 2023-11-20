5da17c745cc99c8a599fe6a177a62179e2f17ac0
RhoxGSAINT <swordknight@naver.com>
2023-11-20 12:56:23 +0900

    Spew missions
    and Beorg loc change

--- script/campaign/cr_combi_expanded/mod/rhox_spew.lua
+++ script/campaign/cr_combi_expanded/mod/rhox_spew.lua
@@ -1,4 +1,15 @@
-
+core:add_listener(
+    "rhox_spew_MissionSucceeded",
+    "MissionSucceeded",
+    function(context)
+        local mission_key = context:mission():mission_record_key();
+        return mission_key == "rhox_spew_survival_mission" and context:faction():name() == "cr_nur_tide_of_pestilence"
+    end,
+    function(context)
+        cm:trigger_mission("cr_nur_tide_of_pestilence", "rhox_spew_survival_mission", true)
+    end,
+    true
+);
 
 
 cm:add_first_tick_callback(
